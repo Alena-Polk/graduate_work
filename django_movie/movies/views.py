723fb5f751d21d83065d9862ad41d0675d8b33ae
Alena-Polk <alenushkapolkina@yandex.ru>
2023-08-06 17:13:33 +0300

    Создан фильтр для вывода уникальных аниме по жанрам и годам.

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,19 +1,29 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import Movie, Category, Actor
+from .models import Movie, Category, Actor, Genre
 from .forms import ReviewForm
 
 
-class MoviesView(ListView):
-    # Список фильмов
+class GenreYear:
+    # Жанры и года выхода аниме
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year").distinct()
+
+
+class MoviesView(GenreYear, ListView):
+    # Список аниме
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(DetailView):
-    # Полное описание фильма
+class MovieDetailView(GenreYear, DetailView):
+    # Полное описание аниме
     model = Movie
     slug_field = "url"
 
@@ -32,8 +42,18 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
-    #Вывод информации о персонаже
+class ActorView(GenreYear, DetailView):
+    # Вывод информации о персонаже
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = "name"
+
+
+class FilterMoviesView(GenreYear, ListView):
+    # Фильтр аниме
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre"))
+        ).distinct()
+        return queryset
